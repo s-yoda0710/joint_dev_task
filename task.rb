996e73970390e8579a8533e12f9aa27f84ab486e
晋也 <s.yoda0710@gmail.com>
2021-08-06 19:19:13 +0900

    Q7,Q8,Q11に破壊的メソッドを適用

--- task.rb
+++ task.rb
@@ -55,6 +55,7 @@ def q6
   #   new_numbers << number * 10
   # end
   # p new_numbers
+
   numbers1 = numbers1.map { |n| n * 10 }
   p numbers1
 end
@@ -63,16 +64,16 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array = array.map { |n| n.to_i }
+  # array = array.map { |n| n.to_i }
   # 以下は変更しないで下さい
-  p array
+  p array.map!(&:to_i)
 end
 
 def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
@@ -104,9 +105,13 @@ end
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
   # 以下に回答を記載
-  uniq_sports = sports.flatten.uniq
-  uniq_sports.each.with_index(1) do |uniq_sport, i|
-    puts "No#{i} #{uniq_sport}"
+  # uniq_sports = sports.flatten.uniq
+  # uniq_sports.each.with_index(1) do |uniq_sport, i|
+  #   puts "No#{i} #{uniq_sport}"
+  # end
+  puts "ユーザーの趣味一覧"
+  sports.flatten!.uniq!.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
   end
 end
 
