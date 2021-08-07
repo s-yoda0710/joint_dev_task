f15356d58db14668d83c36188fe638d4aeb3fdce
晋也 <s.yoda0710@gmail.com>
2021-08-07 09:31:27 +0900

    Q15,Q16を回答

--- task.rb
+++ task.rb
@@ -152,7 +152,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  puts data1.key?(:age) ? "OK" : "NG"
+  puts data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -164,7 +165,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
