84a8fd82d907ba09c6f1297c94a9c49de6d7509e
晋也 <s.yoda0710@gmail.com>
2021-08-26 20:58:22 +0900

    Q17~Q19を回答

--- task.rb
+++ task.rb
@@ -172,7 +172,18 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~TEXT
+      名前:#{@name}
+      年齢:#{@age}
+      性別:#{@gender}
+    TEXT
+  end
 end
 
 def q17
@@ -187,7 +198,13 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def introduce
+    @age >= 20 ? "こんにちは、#{@name}と申します。宜しくお願いします。" : "はいさいまいど〜、#{@name}です！"
+  end
 end
 
 def q18
@@ -201,9 +218,12 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  attr_reader :name
+  def initialize(name:)
     @name = name
+  end
+  def name
+    puts @name
   end
 end
 
